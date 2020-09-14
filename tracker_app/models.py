fceee1e235fa4901ff5ad689ff00631678ded39a
jtay0030 <jtay0030@student.monash.edu>
2020-09-14 22:46:23 +1000

    changed models.py (unfinished)

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Group(models.Model):
+    groupName = models.charField(max_length=30)
+    unitCode = models.charField(max_length=7)
+
+class TaskCategory(models.Model):
+    categoryName = models.charField(max_length=30)
+    description = models.charField(max_length=1000)
+    
+    submitted = models.BooleanField()
