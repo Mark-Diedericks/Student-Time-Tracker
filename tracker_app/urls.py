1f1eb0e8e2926141a89cbdd0b2afe978d84956fe
Mark-Diedericks <Mark-Diedericks@users.noreply.github.com>
2020-09-15 21:25:19 +1000

    Remove orl pattern

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -4,9 +4,10 @@ from tracker_app import views
 app_name = 'tracker_app'
 urlpatterns = [
     path('', views.index, name='index'),
+    
     path('signin/', views.login, name='signin'),
     path('signup/', views.signUp, name='signup'),
-    path('login/', views.login, name='login'), # Pretty sure django already has a login page built-in so this is redundant...
+
     path('dashboard/', views.userdash, name='userdash'),
     path('dashboard/<int:group_id>/', views.groupdash, name='groupdash'),
 ]
