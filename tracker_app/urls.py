e114d8231f631f7e013c7646e8bafa7c7dec4ff7
sloo0007 <sloo0007@student.monash.edu>
2020-09-15 21:41:33 +1000

    login fixes

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -5,8 +5,8 @@ app_name = 'tracker_app'
 urlpatterns = [
     path('', views.index, name='index'),
     
-    path('signin/', views.login, name='signin'),
-    path('signup/', views.signUp, name='signUp'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signUp, name='signup'),
 
     path('dashboard/', views.userdash, name='userdash'),
     path('dashboard/<int:group_id>/', views.groupdash, name='groupdash'),
