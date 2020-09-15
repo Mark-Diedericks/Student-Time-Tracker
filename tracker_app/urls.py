a6e1621a7724535883b2ba249bd881fedf524af4
Mark-Diedericks <Mark-Diedericks@users.noreply.github.com>
2020-09-15 12:10:50 +1000

    Setup url patterns for dashboards

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from tracker_app import views
 
 urlpatterns = [
-    path('', views.login, name='Login'),
+    path('', views.login, name='login'),
+    path('dashboard/', views.userdash, name='userdash'),
+    path('dashboard/<int:group_id>/', views.groupdash, name='groupdash'),
 ]
--- tracker_app/views.py
+++ tracker_app/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def login(request):
-    return HttpResponse("Login page!")
+    return HttpResponse("Login page!")      # Pretty sure django already has a login page built-in so this is redundant...
+
+def userdash(request):
+    return HttpResponse("User dashboard")
+
+def groupdash(request):
+    return HttpResponse("Group dashboard")
