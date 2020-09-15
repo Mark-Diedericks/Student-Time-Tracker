da9ddb868dfdd38f9a62b52ebd370d4646f9216a
Mark-Diedericks <Mark-Diedericks@users.noreply.github.com>
2020-09-15 15:21:02 +1000

    Layout of base template

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -2,13 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Group(models.Model):
-    groupName = models.charField(max_length=30)
-    unitCode = models.charField(max_length=7)
+    groupName = models.CharField(max_length=30)
+    unitCode = models.CharField(max_length=7)
 
     ## 'users' manytomany field of users
 
 class TaskCategory(models.Model):
-    categoryName = models.charField(max_length=30)
-    description = models.charField(max_length=1000)
+    categoryName = models.CharField(max_length=30)
+    description = models.CharField(max_length=1000)
     
     submitted = models.BooleanField()
--- tracker_app/views.py
+++ tracker_app/views.py
@@ -1,5 +1,5 @@
 from django.urls import reverse
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import redirect, render, get_object_or_404
 
 from tracker_app import models
@@ -9,9 +9,9 @@ def index(request):
     # https://docs.djangoproject.com/en/3.1/topics/auth/default/#auth-web-requests
 
     if (request.user is not None) and (request.user.is_authenticated):
-        return redirect('dashboard/')      # We have a user, goto dashboard
+        return redirect('/dashboard/')      # We have a user, goto dashboard
     else:
-        return redirect('login/')          # We don't have a user, goto login
+        return redirect('/login/')          # We don't have a user, goto login
 
 
 
@@ -23,29 +23,30 @@ def login(request):
 
 
 def userdash(request):
-    if (request.user is None) or (not request.user.is_authenticated):       # Always ensure we have a user
-        return redirect('login/')
+    #if (request.user is None) or (not request.user.is_authenticated):       # Always ensure we have a user
+    #    return redirect('/login/')
 
     try:
-        g = models.Group.objects.filter(users__contains=request.user)
+        #g = list(models.Group.objects.filter(users__contains=request.user))      # Get QuerySet of all groups the user belongs to
+        g = []
     except:
-        print()
+        raise Http404("Could not get User's groups")
     else:
-        return render(request, 'tacker_app/trmplates/userdash.html', {'groups': g})
+        return render(request, 'userdash.html', {'groups': g})
 
 
 
 
 def groupdash(request, group_id):
-    if (request.user is None) or (not request.user.is_authenticated):       # Always ensure we have a user
-        return redirect('login/')
+    #if (request.user is None) or (not request.user.is_authenticated):       # Always ensure we have a user
+    #    return redirect('/login/')
 
     try:                                        # Attempt to get the group from the primary-key (id)
         g = models.Group.objects.get(pk=group_id)
 
-    except models.Group.DoesNotExist:           # Group doesn't exist, go back to userdash 
+    except:                                     # Group doesn't exist, go back to userdash 
         print("Group does not exist ", group_id)
-        return redirect('dashboard/')
+        return redirect('/dashboard/')
 
     else:                                       # Group was found, 
-        return render(request, 'tracker_app/templates/groupdash.html', {'group': g})
+        return render(request, 'groupdash.html', {'group': g})
