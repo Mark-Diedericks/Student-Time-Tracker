00ff5d66851264760bbe97ab11e2e50db037fd37
Mark-Diedericks <Mark-Diedericks@users.noreply.github.com>
2020-09-15 13:08:08 +1000

    Setup of views, index redirect user auth

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from tracker_app import views
 
+app_name = 'tracker_app'
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('', views.index, name='index'),
+
+    path('login/', views.login, name='login'), # Pretty sure django already has a login page built-in so this is redundant...
     path('dashboard/', views.userdash, name='userdash'),
     path('dashboard/<int:group_id>/', views.groupdash, name='groupdash'),
 ]
--- tracker_app/views.py
+++ tracker_app/views.py
@@ -1,12 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 
 # Create your views here.
+def index(request):
+    # https://docs.djangoproject.com/en/3.1/topics/auth/default/#auth-web-requests
+
+    if (request.user is not None) and (request.user.is_authenticated):
+        return redirect('dashboard/')      # We have a user, goto dashboard
+    else:
+        return redirect('login/')          # We don't have a user, goto login
+
+
+
+
 def login(request):
-    return HttpResponse("Login page!")      # Pretty sure django already has a login page built-in so this is redundant...
+    return HttpResponse('Login page')
+
+
+
 
 def userdash(request):
+    if (request.user is None) or (not request.user.is_authenticated):       # Always ensure we have a user
+        return redirect('login/')
+
+
+
+
     return HttpResponse("User dashboard")
 
-def groupdash(request):
+
+
+
+def groupdash(request, group_id):
+    if (request.user is None) or (not request.user.is_authenticated):       # Always ensure we have a user
+        return redirect('login/')
+
+
+
+
     return HttpResponse("Group dashboard")
