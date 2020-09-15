319921c294aa2366c1a9684ef154910c5b62f7a5
jtay0030 <jtay0030@student.monash.edu>
2020-09-15 20:19:11 +1000

    Models updated for new UML

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -1,14 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Group(models.Model):
-    groupName = models.charField(max_length=30)
-    unitCode = models.charField(max_length=7)
-
-    ## 'users' manytomany field of users
+    groupName = models.CharField(max_length = 30)
+    unitCode = models.CharField(max_length = 7)
+    members = models.ManyToManyField(User, through = "GroupMember")
 
 class TaskCategory(models.Model):
-    categoryName = models.charField(max_length=30)
-    description = models.charField(max_length=1000)
-    
+    categoryName = models.CharField(max_length = 30)
+    description = models.TextField()
     submitted = models.BooleanField()
+    submittedBy = models.ForeignKey(MemberEntry)
+    date = models.DateField()
+
+class GroupMember(models.Model):
+    ## user attribute (not quite sure how to store. models.User()? models.ForeignKey(User())?)
+    roles = models.ListCharField(
+        base_field = models.CharField(max_length = 20),
+        size = 10, # at most you can have 10 roles (had to specify this param)
+        max_length = 200 # 10 slots * 20 chars
+    )
+
+class MemberEntry(models.Model):
+    groupMember = models.ForeignKey(GroupMember)
+    hoursSpent = models.IntegerField()
