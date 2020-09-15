b67baaffe9ddf7e86722f3644bf167fee1eb13cd
sloo0007 <sloo0007@student.monash.edu>
2020-09-15 20:54:30 +1000

    url change

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -3,8 +3,9 @@ from tracker_app import views
 
 app_name = 'tracker_app'
 urlpatterns = [
+    path('', views.login, name='Login'),
+    path('signUp/', views.signUp, name='signUp'),
     path('', views.index, name='index'),
-
     path('login/', views.login, name='login'), # Pretty sure django already has a login page built-in so this is redundant...
     path('dashboard/', views.userdash, name='userdash'),
     path('dashboard/<int:group_id>/', views.groupdash, name='groupdash'),
