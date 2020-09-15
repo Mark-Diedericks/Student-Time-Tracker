0faa67c3d1e1a96afea90afb4a5ff3cd8f44de19
jtay0030 <jtay0030@student.monash.edu>
2020-09-15 21:15:45 +1000

    Update models.py

--- tracker_app/models.py
+++ tracker_app/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
+
 
 # Create your models here.
+class GroupMember(models.Model):
+    ## user attribute (not quite sure how to store. models.User()? models.ForeignKey(User())?)
+    roles = models.ArrayField(
+        models.CharField(max_length = 20),
+        size = 10, # at most you can have 10 roles (had to specify this param)
+    )
+    
+
+class MemberEntry(models.Model):
+    groupMember = models.ForeignKey(GroupMember)
+    hoursSpent = models.IntegerField()
+
+
 class Group(models.Model):
     groupName = models.CharField(max_length = 30)
     unitCode = models.CharField(max_length = 7)
-    members = models.ManyToManyField(GroupMember)
-    tasks = models.ManyToManyField(TaskCategory)
+    members = models.ManyToManyField(User, through = "GroupMember")
+
 
 class TaskCategory(models.Model):
     categoryName = models.CharField(max_length = 30)
@@ -14,17 +29,3 @@ class TaskCategory(models.Model):
     submitted = models.BooleanField()
     submittedBy = models.ForeignKey(MemberEntry)
     date = models.DateField()
-
-class GroupMember(models.Model):
-    ## user attribute (not quite sure how to store. models.User()? models.ForeignKey(User())?)
-    person = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
-
-    roles = models.ListCharField(
-        base_field = models.CharField(max_length = 20),
-        size = 10, # at most you can have 10 roles (had to specify this param)
-        max_length = 200 # 10 slots * 20 chars
-    )
-
-class MemberEntry(models.Model):
-    groupMember = models.ForeignKey(GroupMember)
-    hoursSpent = models.IntegerField()
