2c4650e2b22c0076d20c531337e0d2daeebd24cf
sloo0007 <sloo0007@student.monash.edu>
2020-09-16 16:47:01 +1000

    Login, register and dashboard profile+logout

--- users/views.py
+++ users/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            response = redirect('userdash')
+            messages.success(request, f'Your account has been created! You are now able to log in!')
+            #Needs to be changed from register to dashboard once user is done
+            response = redirect('login')
             return response
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request,'users/register.html', {'form': form})
+
+def profile(request):
+    return render(request,'users/profile.html')
