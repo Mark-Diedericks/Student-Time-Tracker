83909f678ba6d566a5618d638abdd4ba7b22e853
mgoy0001@student.monash.edu <mgoy0001@student.monash.edu>
2020-09-18 01:36:01 +1000

    CreateGroup redirects to dashboard

--- tracker_app/views.py
+++ tracker_app/views.py
@@ -29,7 +29,11 @@ def CreateGroup(request):
         form = forms.GroupForm(request.POST)
         if form.is_valid():
             group = form.save()
-            return redirect('/upload-csv/{}/'.format(group.id))
+            groupName = form.cleaned_data.get('groupName')
+            unitCode = form.cleaned_data.get('unitCode')
+            messages.success(request,'Group has been created!!')
+            #return redirect('/upload-csv/{}/'.format(group.id))
+            return redirect('/dashboard/')
     else:
         form = forms.GroupForm()
         return render(request,'creategroup.html', {'form': form})
