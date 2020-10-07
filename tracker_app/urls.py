102053677c138db549e63deba5fe4e64bff517b4
Mark-Diedericks <Mark-Diedericks@users.noreply.github.com>
2020-10-07 19:06:20 +1100

    Better responsiveness from base.html

--- tracker_app/urls.py
+++ tracker_app/urls.py
@@ -10,7 +10,4 @@ urlpatterns = [
     path('addmember/<int:group_id>/',views.addmember,name='addmember'),
     path('dashboard/<int:group_id>/', views.groupdash, name='groupdash'),
     path('dashboard/<int:group_id>/<int:mem_id>/', views.groupdash, name='groupmemdash'),
-
-    #path('add_members/<int:group_id>/',views.add_members,name='add_members'),
-    #path('upload-csv/<int:group_id>/',views.members_upload,name="members_upload")
 ]
