db647b0bef1326415654d9b8815debf86f0a3c19
Mark-Diedericks <Mark-Diedericks@users.noreply.github.com>
2020-10-28 12:48:50 +1100

    User Story 10: Adding/selecting roles - Refresh groupdash if leader/owner roles are altered - Added refresh flag to setroles JSON POST request - Fixed new roles being created with leader permissions. - No known issues.

--- tracker_app/indv_views/manageroles.py
+++ tracker_app/indv_views/manageroles.py
@@ -19,7 +19,7 @@ def addrole(request, group_id):
     try:
         role_name = request.POST['newRoleName']             # Get name of new role
         g = models.Group.objects.get(pk = group_id)
-        role = models.MemberRole(name=role_name, is_owner=False, is_leader=True, group = g)
+        role = models.MemberRole(name=role_name, is_owner=False, is_leader=False, group = g)
         role.save()                                         # Create the new role, no special permissions
     except:
         return JsonResponse({ 'status': 'bad' })
@@ -41,18 +41,27 @@ def setroles(request, group_id, mem_id):
         mem = models.GroupMember.objects.get(id = mem_id)
         roles = list(models.MemberRole.objects.filter(group = g))
 
+        req_ref = False                                 # Whether a page refresh is required (changed abilities)
+
+        og_roles = list(mem.roles.all())
         mem.roles.clear()                               # Remove all roles from the user, to add them back in
         rArr = []                                       # Get the names of each role the member has
 
         # Add back in selected roles and create prettyroles
         for r in roles:
             if str(r.pk) in selected_roles:
+                if (r not in og_roles) and (r.is_owner or r.is_leader):
+                    req_ref = True                      # Added owner/leader role, thus refresh
+
                 mem.roles.add(r)                        # Add back in the selected roles
                 rArr.append(r.name)
+            else:
+                if (r in og_roles) and (r.is_owner or r.is_leader):
+                    req_ref = True                      # Removed owner/leader role, thus refresh
 
         # Set the pretty names for roles and member name
         prettyroles = ', '.join(rArr)
     except:
         return JsonResponse({ 'status': 'bad' })
 
-    return JsonResponse({'status': 'ok', 'prettyroles': prettyroles})
+    return JsonResponse({'status': 'ok', 'refresh': req_ref, 'prettyroles': prettyroles})
